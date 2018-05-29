a05edef9f5ab022d2a806e00d06f841b7d519b6e
Miosolo <xht2012.jn@gmail.com>
2018-05-30 02:58:05 +0800

    更正了自写swap的重名问题

--- ch3/ex3-10.cpp
+++ ch3/ex3-10.cpp
@@ -1,80 +1,80 @@
 #include <stdio.h>
 #include <string.h>
 
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+void MySwap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
 int main(void) {
-    int pallet[6][3];
-    memset(pallet, -1, sizeof(pallet));
+  int pallet[6][3];
+  memset(pallet, -1, sizeof(pallet));
 
-    while (scanf("%d", &pallet[0][1]) == 1) {
-        for (int *p = &pallet[0][2]; p <= &pallet[5][2]; p++) {
-            if ((p - pallet[0]) % 3 == 0) {
-                continue;
-            } else if ((p - pallet[0]) % 3 == 2) {
-                scanf("%d", p);
-                if (*p > *(p - 1)) swap(p, p - 1);
-            } else {
-                scanf("%d", p);
-            }
-        }
-
-        for (int *pp = pallet[0], pairCount = 0; pp <= pallet[5]; pp += 3) {
-            if (*pp != -1) {
-                continue;
-            } else {
-                for (int *ppt = pp + 3; ppt <= pallet[5]; ppt += 3) {
-                    if (*(ppt + 1) == *(pp + 1) && *(ppt + 2) == *(pp + 2)) {
-                        *pp = *ppt = pairCount++;
-                        break;
-                    }
-                }
-            }
-        }
-
-        bool paired = true;
-        for (int *pp = pallet[0]; pp <= pallet[5]; pp += 3) {
-            if (*pp == -1) {
-                paired = false;
-                break;
-            }
-        }
-
-        if (paired) {
-            int vp[3][2] = {0};
-            for (int *pp = pallet[0]; pp <= pallet[5]; pp += 3) {
-                if (vp[*pp][0] == 0) memcpy(vp[*pp], pp + 1, 2 * sizeof(int));
-            }
-
-            bool allMatch = true;
-            for (int i = 0; i < 3; i++) {
-                int *pl = vp[0];
-
-                while (!*pl) pl++;
-                for (int *plt = pl + 1; plt <= &vp[2][1]; plt++) {
-                    if (*plt == *pl) {
-                        *plt = *pl = 0;
-                        break;
-                    }
-                }
-            }
-            for (int *pl = vp[0]; pl <= &vp[2][1]; pl++) {
-                if (*pl) allMatch = false;
-            }
-
-            if (allMatch) {
-                printf("POSSIBLE\n");
-                memset(pallet, -1, sizeof(pallet));
-                continue;
-            }
-        }
-        printf("IMPOSSIBLE\n");
-        memset(pallet, -1, sizeof(pallet));
+  while (scanf("%d", &pallet[0][1]) == 1) {
+    for (int *p = &pallet[0][2]; p <= &pallet[5][2]; p++) {
+      if ((p - pallet[0]) % 3 == 0) {
+        continue;
+      } else if ((p - pallet[0]) % 3 == 2) {
+        scanf("%d", p);
+        if (*p > *(p - 1)) MySwap(p, p - 1);
+      } else {
+        scanf("%d", p);
+      }
     }
 
-    return 0;
+    for (int *pp = pallet[0], pairCount = 0; pp <= pallet[5]; pp += 3) {
+      if (*pp != -1) {
+        continue;
+      } else {
+        for (int *ppt = pp + 3; ppt <= pallet[5]; ppt += 3) {
+          if (*(ppt + 1) == *(pp + 1) && *(ppt + 2) == *(pp + 2)) {
+            *pp = *ppt = pairCount++;
+            break;
+          }
+        }
+      }
+    }
+
+    bool paired = true;
+    for (int *pp = pallet[0]; pp <= pallet[5]; pp += 3) {
+      if (*pp == -1) {
+        paired = false;
+        break;
+      }
+    }
+
+    if (paired) {
+      int vp[3][2] = {0};
+      for (int *pp = pallet[0]; pp <= pallet[5]; pp += 3) {
+        if (vp[*pp][0] == 0) memcpy(vp[*pp], pp + 1, 2 * sizeof(int));
+      }
+
+      bool allMatch = true;
+      for (int i = 0; i < 3; i++) {
+        int *pl = vp[0];
+
+        while (!*pl) pl++;
+        for (int *plt = pl + 1; plt <= &vp[2][1]; plt++) {
+          if (*plt == *pl) {
+            *plt = *pl = 0;
+            break;
+          }
+        }
+      }
+      for (int *pl = vp[0]; pl <= &vp[2][1]; pl++) {
+        if (*pl) allMatch = false;
+      }
+
+      if (allMatch) {
+        printf("POSSIBLE\n");
+        memset(pallet, -1, sizeof(pallet));
+        continue;
+      }
+    }
+    printf("IMPOSSIBLE\n");
+    memset(pallet, -1, sizeof(pallet));
+  }
+
+  return 0;
 }
--- ch3/ex3-5.cpp
+++ ch3/ex3-5.cpp
@@ -5,106 +5,104 @@
 char frame[5][5];
 char input[500];
 
-void swap(char* pa, char* pb)
-{
-    char t = *pa;
-    *pa = *pb;
-    *pb = t;
+void MySwap(char* pa, char* pb) {
+  char t = *pa;
+  *pa = *pb;
+  *pb = t;
 
-    return;
+  return;
 }
 
-int main(void)
-{
-    for (int kase = 1;; kase++) {
-        memset(input, 0, sizeof(input));
+int main(void) {
+  for (int kase = 1;; kase++) {
+    memset(input, 0, sizeof(input));
 
-        char* pi = input;
-        for (pi = input; pi < input + 25; pi += 5) {
-            fgets(pi, 30, stdin);
-            if (*pi == 'Z') {
-                return 0;
-            }
-        }
-        pi = input + 25;
-        while (1) {
-            scanf("%s", pi);
-            while (*pi != 0) {
-                pi++;
-            }
-            if (*(pi - 1) == '0') {
-                break;
-            } else {
-                pi++;
-            }
-        }
-        getchar();
+    char* pi = input;
+    for (pi = input; pi < input + 25; pi += 5) {
+      fgets(pi, 30, stdin);
+      if (*pi == 'Z') {
+        return 0;
+      }
+    }
+    pi = input + 25;
+    while (1) {
+      scanf("%s", pi);
+      while (*pi != 0) {
+        pi++;
+      }
+      if (*(pi - 1) == '0') {
+        break;
+      } else {
+        pi++;
+      }
+    }
+    getchar();
 
-        char* pBlank = NULL;
-        pi = input;
-        for (char *pf = frame[0]; pf <= &frame[4][4]; pf++, pi++) {
-            *pf = *pi;
-            if (*pf == ' ' || *pf == '\n') {
-                *pf = ' ';
-                pBlank = pf;
-            }
-        }
-
-        bool noConf = false;
-        pi = input + 25;
-        while (!noConf && *pi != '0') {
-            switch (*pi) {
-            case 'A':
-                if (pBlank - 5 >= frame[0]) {
-                    swap(pBlank, pBlank - 5);
-                    pBlank -= 5;
-                } else
-                    noConf = true;
-                break;
-
-            case 'B':
-                if (pBlank + 5 <= &frame[4][4]) {
-                    swap(pBlank, pBlank + 5);
-                    pBlank += 5;
-                } else
-                    noConf = true;
-                break;
-
-            case 'L':
-                if ((pBlank - frame[0]) % 5 != 0) {
-                    swap(pBlank, pBlank - 1);
-                    pBlank--;
-                } else
-                    noConf = true;
-                break;
-
-            case 'R':
-                if ((pBlank - frame[0]) % 5 != 4) {
-                    swap(pBlank, pBlank + 1);
-                    pBlank++;
-                } else
-                    noConf = true;
-                break;
-            }
-            pi++;
-        }
-
-        if (kase != 1) {
-            putchar('\n');
-        }
-        printf("Puzzle #%d:\n", kase);
-        if (noConf)
-            printf("This puzzle has no final configuration.\n");
-        else {
-            for (char* pf = frame[0]; pf <= &frame[4][4]; pf++) {
-                putchar(*pf);
-                if ((pf - frame[0]) % 5 == 4)
-                    putchar('\n');
-                else
-                    putchar(' ');
-            }
-        }
+    char* pBlank = NULL;
+    pi = input;
+    for (char* pf = frame[0]; pf <= &frame[4][4]; pf++, pi++) {
+      *pf = *pi;
+      if (*pf == ' ' || *pf == '\n') {
+        *pf = ' ';
+        pBlank = pf;
+      }
     }
 
-    return 0;
+    bool noConf = false;
+    pi = input + 25;
+    while (!noConf && *pi != '0') {
+      switch (*pi) {
+        case 'A':
+          if (pBlank - 5 >= frame[0]) {
+            MySwap(pBlank, pBlank - 5);
+            pBlank -= 5;
+          } else
+            noConf = true;
+          break;
+
+        case 'B':
+          if (pBlank + 5 <= &frame[4][4]) {
+            MySwap(pBlank, pBlank + 5);
+            pBlank += 5;
+          } else
+            noConf = true;
+          break;
+
+        case 'L':
+          if ((pBlank - frame[0]) % 5 != 0) {
+            MySwap(pBlank, pBlank - 1);
+            pBlank--;
+          } else
+            noConf = true;
+          break;
+
+        case 'R':
+          if ((pBlank - frame[0]) % 5 != 4) {
+            MySwap(pBlank, pBlank + 1);
+            pBlank++;
+          } else
+            noConf = true;
+          break;
+      }
+      pi++;
+    }
+
+    if (kase != 1) {
+      putchar('\n');
+    }
+    printf("Puzzle #%d:\n", kase);
+    if (noConf)
+      printf("This puzzle has no final configuration.\n");
+    else {
+      for (char* pf = frame[0]; pf <= &frame[4][4]; pf++) {
+        putchar(*pf);
+        if ((pf - frame[0]) % 5 == 4)
+          putchar('\n');
+        else
+          putchar(' ');
+      }
+    }
+  }
+
+  return 0;
 }
